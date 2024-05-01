1106381587805d334f7eaa2d5fcf21399562b795
arusafa <arusafacollege@gmail.com>
2024-05-01 14:36:16 -0400

    User_service_ Impl has been updated

--- src/main/java/com/aru/services/UserServiceImpl.java
+++ src/main/java/com/aru/services/UserServiceImpl.java
@@ -1,25 +1,49 @@
 package com.aru.services;
 
+import com.aru.configuration.JwtProvider;
 import com.aru.models.User;
+import com.aru.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class UserServiceImpl implements UserService{
-    @Override
-    public User findUserProfileByJwt(String jwt) throws Exception {
-        return null;
-    }
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
+    public User findUserProfileByJwt(String jwt) throws Exception {
+
+        String email = JwtProvider.getEmailFromToken(jwt);
+        return findUserByEmail(email);
+    }
+    @Override
     public User findUserByEmail(String email) throws Exception {
-        return null;
+
+        User user = userRepository.findByEmail(email);
+
+        if (user==null){
+            throw new Exception("User not found!");
+        }
+        return user;
     }
 
     @Override
     public User findUserById(Long userId) throws Exception {
-        return null;
+
+        Optional<User> optionalUser = userRepository.findById(userId);
+
+        if (optionalUser.isEmpty()){
+            throw new Exception("User not found!");
+        }
+        return optionalUser.get();
     }
 
     @Override
     public User updateUsersProjectSize(User user, int number) throws Exception {
-        return null;
+
+        user.setProjectSize(user.getProjectSize()+number);
+        return userRepository.save(user);
     }
 }
