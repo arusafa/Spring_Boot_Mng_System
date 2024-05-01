728f5553ef47924e7a691a89239a2fe74768be7c
arusafa <arusafacollege@gmail.com>
2024-05-01 04:44:29 -0400

    models updated

---
+++ src/main/java/com/aru/models/Comment.java
@@ -0,0 +1,29 @@
+package com.aru.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String content;
+    private LocalDateTime createdDateTime;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Issue issue;
+
+}
