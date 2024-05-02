c7df3bacadc50fe95b68af9b4e08ea3c6afb0507
arusafa <arusafacollege@gmail.com>
2024-05-02 02:55:43 -0400

    Payment-Controller updated

--- src/main/java/com/aru/controllers/PaymentController.java
+++ src/main/java/com/aru/controllers/PaymentController.java
@@ -1,9 +1,15 @@
 package com.aru.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/payment")
 public class PaymentController {
+
+    @Value("${STRIPE_API_KEY}")
+    String stripePublicKey;
+
+
 }
