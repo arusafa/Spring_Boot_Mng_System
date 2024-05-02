1aa553ff18b7e06c7004c9cd428e53a37ae0e79a
arusafa <arusafacollege@gmail.com>
2024-05-01 20:05:37 -0400

    InviteRequest has been created

---
+++ src/main/java/com/aru/requests/InviteRequest.java
@@ -0,0 +1,14 @@
+package com.aru.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InviteRequest {
+
+    private Long projectId;
+    private String email;
+}
