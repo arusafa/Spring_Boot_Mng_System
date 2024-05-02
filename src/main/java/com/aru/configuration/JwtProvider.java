eeae0b6c85cd6798d9da052d93bed0d344c21eb0
arusafa <arusafacollege@gmail.com>
2024-05-02 04:16:18 -0400

    Jwt updated in order to fix Bearer token bug

--- src/main/java/com/aru/configuration/JwtProvider.java
+++ src/main/java/com/aru/configuration/JwtProvider.java
@@ -25,6 +25,8 @@ public class JwtProvider {
 
     public static String getEmailFromToken(String jwt){
 
+        // Bearer token
+        jwt = jwt.substring(7);
         Claims claims = Jwts.parser().setSigningKey(keys).build().parseClaimsJws(jwt).getBody();
         return String.valueOf(claims.get("email"));
     }
