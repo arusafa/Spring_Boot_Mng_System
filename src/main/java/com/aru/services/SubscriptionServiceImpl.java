193f23ceab2f662516bff0f3a354ec92909a5bc4
arusafa <arusafacollege@gmail.com>
2024-05-02 01:29:55 -0400

    Subscription-Service-Impl has been updated

--- src/main/java/com/aru/services/SubscriptionServiceImpl.java
+++ src/main/java/com/aru/services/SubscriptionServiceImpl.java
@@ -33,16 +33,42 @@ public class SubscriptionServiceImpl implements SubscriptionService{
 
     @Override
     public Subscription getUserSubscription(Long userId) throws Exception {
-        return null;
+
+        Subscription subscription = subscriptionRepository.findByUserId(userId);
+
+        if (!isValid(subscription)){
+            subscription.setPlanType(PlanType.FREE);
+            subscription.setSubscriptionEndDate(LocalDate.now().plusMonths(12));
+            subscription.setSubscriptionStartDate(LocalDate.now());
+        }
+        return subscriptionRepository.save(subscription);
     }
 
     @Override
     public Subscription upgradeSubscription(Long userId, PlanType planType) {
-        return null;
+
+        Subscription subscription = subscriptionRepository.findByUserId(userId);
+        subscription.setPlanType(planType);
+        subscription.setSubscriptionStartDate(LocalDate.now());
+
+        if (planType.equals(PlanType.ANNUALLY)) {
+            subscription.setSubscriptionEndDate(LocalDate.now().plusMonths(12));
+        }
+        else{
+            subscription.setSubscriptionEndDate(LocalDate.now().plusMonths(1));
+        }
+        return subscriptionRepository.save(subscription);
     }
 
     @Override
     public boolean isValid(Subscription subscription) {
-        return false;
+
+        if (subscription.getPlanType() == PlanType.FREE) {
+            return true;
+        }
+        LocalDate endDate = subscription.getSubscriptionEndDate();
+        LocalDate currentDate = LocalDate.now();
+
+        return endDate.isAfter(currentDate) || endDate.isEqual(currentDate);
     }
 }
