58416762720745f596bb359b47551e4bf1156e2a
arusafa <arusafacollege@gmail.com>
2024-05-02 01:11:17 -0400

    Subscription-Service-Impl has been updated

--- src/main/java/com/aru/services/SubscriptionServiceImpl.java
+++ src/main/java/com/aru/services/SubscriptionServiceImpl.java
@@ -3,13 +3,32 @@ package com.aru.services;
 import com.aru.models.PlanType;
 import com.aru.models.Subscription;
 import com.aru.models.User;
+import com.aru.repositories.SubscriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class SubscriptionServiceImpl implements SubscriptionService{
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
+
     @Override
     public Subscription createSubscription(User user) {
-        return null;
+
+        Subscription subscription = new Subscription();
+        subscription.setUser(user);
+        subscription.setSubscriptionStartDate(LocalDate.now());
+        subscription.setSubscriptionEndDate(LocalDate.now().plusMonths(12));
+        subscription.setValid(true);
+        subscription.setPlanType(PlanType.FREE);
+
+        return subscriptionRepository.save(subscription);
     }
 
     @Override
