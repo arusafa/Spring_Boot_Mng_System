fc93f300c893001be3dabc76a316d935b8e094f6
arusafa <arusafacollege@gmail.com>
2024-05-01 23:48:52 -0400

    Comment-controller has been created

---
+++ src/main/java/com/aru/controllers/CommentController.java
@@ -0,0 +1,20 @@
+package com.aru.controllers;
+
+import com.aru.services.CommentService;
+import com.aru.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private UserService userService;
+
+
+}
