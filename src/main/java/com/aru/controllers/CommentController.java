28ed01ac7166d5665f9dcfd15b26385379b0d18d
arusafa <arusafacollege@gmail.com>
2024-05-02 00:01:02 -0400

    Comment-controller has been updated

--- src/main/java/com/aru/controllers/CommentController.java
+++ src/main/java/com/aru/controllers/CommentController.java
@@ -1,10 +1,17 @@
 package com.aru.controllers;
 
+import com.aru.models.Comment;
+import com.aru.models.User;
+import com.aru.requests.CreateCommentRequest;
+import com.aru.responses.MessageResponse;
 import com.aru.services.CommentService;
 import com.aru.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/comments")
@@ -16,5 +23,40 @@ public class CommentController {
     @Autowired
     private UserService userService;
 
+    @PostMapping
+    public ResponseEntity<Comment> createComment(
+            @RequestBody CreateCommentRequest req,
+            @RequestHeader("Authorization") String jwt
+            )throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+        Comment createdComment = commentService.createComment
+                (
+                req.getIssueId(),
+                user.getId(),
+                req.getContent()
+                );
+        return new ResponseEntity<>(createdComment, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<MessageResponse> deleteComment(
+            @PathVariable Long commentId,
+            @RequestHeader("Authorization") String jwt
+            ) throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+        commentService.deleteComment(commentId, user.getId());
+        MessageResponse res = new MessageResponse();
+        res.setMessage("Comment deleted successfully!");
+        return new ResponseEntity<>(res,HttpStatus.OK);
+    }
+
+    @GetMapping("{issueId}")
+    public ResponseEntity<List<Comment>> getCommentsByIssueId(
+            @PathVariable Long issueId) throws Exception {
+        List <Comment> comments = commentService.findCommentByIssueId(issueId);
+        return new ResponseEntity<>(comments,HttpStatus.OK);
+    }
 
 }
