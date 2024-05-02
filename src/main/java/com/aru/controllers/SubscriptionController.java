5b64e4c5c4e8a5e6f13761e1f8bbd781648f5f1a
arusafa <arusafacollege@gmail.com>
2024-05-02 01:39:05 -0400

    Subscription-Controller has been updated

--- src/main/java/com/aru/controllers/SubscriptionController.java
+++ src/main/java/com/aru/controllers/SubscriptionController.java
@@ -1,13 +1,14 @@
 package com.aru.controllers;
 
+import com.aru.models.PlanType;
 import com.aru.models.Subscription;
+import com.aru.models.User;
 import com.aru.services.SubscriptionService;
 import com.aru.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/subscription")
@@ -19,9 +20,28 @@ public class SubscriptionController {
     @Autowired
     private UserService userService;
 
-    public ResponseEntity <Subscription> getUserSubscription(@RequestHeader ("Authorization") String jwt) {
-        return new ResponseEntity<>(null);
+    @GetMapping("/user")
+    public ResponseEntity <Subscription> getUserSubscription(
+            @RequestHeader ("Authorization") String jwt) throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+        Subscription subscription = subscriptionService.getUserSubscription(user.getId());
+
+        return new ResponseEntity<>(subscription, HttpStatus.OK);
     }
+
+    @PatchMapping ("/upgrade")
+    public ResponseEntity <Subscription> upgradeSubscription(
+            @RequestHeader ("Authorization") String jwt,
+            @RequestParam PlanType planType) throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+        Subscription subscription = subscriptionService.upgradeSubscription(user.getId(), planType);
+
+        return new ResponseEntity<>(subscription, HttpStatus.OK);
+    }
+
+
 
 
 
