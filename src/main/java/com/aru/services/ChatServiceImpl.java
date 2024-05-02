722c3a7ed6d6191f781dd85f84869deb300a8e63
arusafa <arusafacollege@gmail.com>
2024-05-02 04:27:34 -0400

    ChatServiceImpl updated

--- src/main/java/com/aru/services/ChatServiceImpl.java
+++ src/main/java/com/aru/services/ChatServiceImpl.java
@@ -2,11 +2,13 @@ package com.aru.services;
 
 import com.aru.models.Chat;
 import com.aru.repositories.ChatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatServiceImpl implements ChatService{
 
+    @Autowired
     private ChatRepository chatRepository;
 
     @Override
